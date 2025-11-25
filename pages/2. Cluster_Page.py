6cdbed5a0affcab9757586218466819ca9f19c91
bprtdaniel <daniel@boppert.net>
2025-11-24 21:27:28 -0500

    m

--- pages/2. Cluster_Page.py
+++ pages/2. Cluster_Page.py
@@ -182,15 +182,12 @@ if st.button("Show My Lists on a Map"):
     m = plot_clusters_interactive(top10_df)
     html(m._repr_html_(), width=900, height=600)
 
-    
-    
-    
-st.markdown("---")
-st.markdown(
-    """
-    <div style='text-align: center; color: gray; padding: 20px;'>
-        <p>Voter Clustering App | Georgetown University | Daniel Boppert | 2024</p>
-    </div>
-    """,
-    unsafe_allow_html=True
+    st.markdown("---")
+    st.markdown(
+        """
+        <div style='text-align: center; color: gray; padding: 20px;'>
+            <p>Voter Clustering App | Georgetown University | Daniel Boppert | 2024</p>
+        </div>
+        """,
+        unsafe_allow_html=True
 )
