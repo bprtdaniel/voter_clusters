13a4af4d7583c574d15a91517bed119499adcfa8
bprtdaniel <daniel@boppert.net>
2025-11-24 20:13:21 -0500

    rename

--- pages/1. Whats_Is_a_Voter_List.py
+++ pages/1. Whats_Is_a_Voter_List.py
@@ -40,7 +40,7 @@ st.markdown("---")
 st.markdown(
     """
     <div style='text-align: center; color: gray; padding: 20px;'>
-        <p>Voter Clustering App | Georgetown University | 2024</p>
+        <p>Voter Clustering App | Georgetown University | Daniel Boppert | 2024</p>
     </div>
     """,
     unsafe_allow_html=True
