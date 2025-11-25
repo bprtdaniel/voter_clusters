e39963d920c2b054157ada1779c07fd1fc245120
bprtdaniel <daniel@boppert.net>
2025-11-24 20:02:54 -0500

    Create Whats_Is_a_Voter_List.py

---
+++ pages/Whats_Is_a_Voter_List.py
@@ -0,0 +1,23 @@
+import streamlit as st
+from PIL import Image
+
+
+st.set_page_config(
+page_title="What is a Voter List!",
+layout="wide"
+)
+
+
+st.title("What is a Voter List?")
+
+st.subheader("I'm glad you asked!")
+
+
+st.write("""For each election, we create so called Voter Targets.
+        Generally, these are eligible we want to speak to about the upcoming election.
+        
+        We rely on the great help of volunteers like you, to talk to as many voters as possible.
+        
+        In order to do so, you need what we call a "Turf List", but we'll just call it a voter list here.
+        
+        Basically, this is a smaller list, taken from our overall voter targets, so we can pass it out to you, so you know who to talk to and where.""")
